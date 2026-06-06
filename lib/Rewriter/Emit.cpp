034e9a9ab9d27807d72fc6856a330c590b25459c
Love Dahlberg <love.dahlberg@hotmail.com>
2026-06-06 03:33:33 +0200

    Add EmitConstantIsland

--- lib/Rewriter/Emit.cpp
+++ lib/Rewriter/Emit.cpp
@@ -10,9 +10,217 @@
 
 #include <Rewriter/Emit.h>
 
+#include <ranges>
+
 namespace Rewriter {
 
 void emitDataSections();
+
+/// Represents a continous range of data inside of a function. This ends either
+/// at another chunk of data, which might contain different kind of data and/or
+/// at different alignment, or when executable code starts again.
+struct IslandChunk {
+  /// The start offset where data begins.
+  uint64_t startOffset;
+
+  /// The end offset where either code beings or another data block beings.
+  uint64_t endOffset;
+
+  /// All jump labels inside of the chunk with a offset and symbol pair.
+  std::vector<std::pair<const uint64_t, llvm::MCSymbol *>> jumpLabels;
+
+  /// Relocations inside of the chunk.
+  std::vector<llvm::bolt::Relocation> relocations;
+};
+
+/// Go through all constant islands in the current function and collect them as
+/// island chunks. An island chunk represents a sequential sequence of data that
+/// all hold the same kind of data with the same alignment. One chunk may
+/// immediately be followed by another one in the original function, but this is
+/// still considered a new chunk by LLVM if they are separated by a 'start data'
+/// marker.
+std::vector<IslandChunk>
+BuildIslandChunks(llvm::bolt::BinaryFunction *function) {
+
+  auto &islands = function->getIslandInfo();
+
+  auto getNextBoundaryOrMax = [&](const auto &set,
+                                  const uint64_t startOffset) -> uint64_t {
+    // Get the range greater to or equal the startOffset.
+    auto range = std::ranges::upper_bound(set, startOffset);
+
+    // If offset is too large, set as absolute function end.
+    return range != set.end() ? *range : function->getMaxSize();
+  };
+
+  auto getMapSubrange = [](const auto &map, const uint64_t startOffset,
+                           const uint64_t endOffset) {
+    return std::ranges::subrange(map.lower_bound(startOffset),
+                                 map.lower_bound(endOffset));
+  };
+
+  // Loop through all DataOffsets, which denotes the start of a contiguous block
+  // of data in code. This block of data can contain several labels that the
+  // code can jump to.
+  std::vector<IslandChunk> chunks;
+  for (const auto startOffset : islands.DataOffsets) {
+    IslandChunk chunk;
+
+    chunk.startOffset = startOffset;
+
+    // Calcuate the end offset of this chunk. The end is either:
+    // - The start marker of the nearest other data chunk (which is right next
+    //   to our chunk). This is data of different type and/or alignment.
+    // - The nearest code start marker. We take which ever is closest.
+    //   - This is what is in CodeOffsets, offsets where executable code starts.
+    const auto startOfNextDataBlock =
+        getNextBoundaryOrMax(islands.DataOffsets, startOffset);
+    const auto endOfCurrentDataBlock =
+        getNextBoundaryOrMax(islands.CodeOffsets, startOffset);
+    std::min(startOfNextDataBlock, endOfCurrentDataBlock);
+
+    // If there is no data in the range, skip.
+    if (chunk.startOffset == chunk.endOffset)
+      continue;
+
+    // Get the labels that are inside of our chunk.
+    const auto labelSubRange =
+        getMapSubrange(islands.Offsets, chunk.startOffset, chunk.endOffset);
+    for (const auto &item : labelSubRange) {
+      chunk.jumpLabels.push_back(item);
+    }
+
+    // Get the relocations that are inside of our chunk.
+    const auto relocationSubRange =
+        getMapSubrange(islands.Relocations, chunk.startOffset,
+                       chunk.endOffset) |
+        std::views::values;
+    for (const auto &item : relocationSubRange) {
+      chunk.relocations.push_back(item);
+    }
+
+    chunks.push_back(std::move(chunk));
+  }
+
+  return chunks;
+}
+
+/// Constant islands represents parts of executable code that is actually data.
+/// One function may contain several islands of data through out
+/// the functions. These islands of data can all be placed separately, or
+/// grouped back to back.
+///
+/// Here, we gather them all up and emit them in one big island at the end of
+/// the current function. This is taken from bolt which does this for I-cache
+/// optimization. It doesn't really matter for us here, we just want to emit it
+/// for now. Ideally we control the layout of the function explicitly in passes,
+/// but for now we can keep this as is.
+void emitConstantIsland(llvm::bolt::BinaryContext *binaryContext,
+                        llvm::bolt::BinaryFunction *function,
+                        llvm::MCStreamer *streamer) {
+
+  auto &islands = function->getIslandInfo();
+  if (islands.DataOffsets.empty()) {
+    return;
+  }
+
+  // Go through all constant islands in the function, that are possible spread
+  // out, and store their offset content as a island chunk.
+  const std::vector<IslandChunk> islandChunks = BuildIslandChunks(function);
+
+  // Get the raw contents of the function. We are going to use this to emit
+  // specific sequences of data.
+  const auto FunctionContents =
+      function->getOriginSection()->getContents().substr(
+          function->getAddress() - function->getOriginSection()->getAddress(),
+          function->getMaxSize());
+
+  // TODO, make into a separate function.
+  // Emits the relevant data and relocations within a offset range.
+  auto emitDataInRange = [&](uint64_t currentOffset,
+                             const uint64_t targetEndOffset,
+                             const auto relocations) -> uint64_t {
+    // Emit all relevant relocations.
+    for (const auto &relocation : relocations) {
+      // If there is space in between where the current offset and where the
+      // relocation starts. This simply means that we have raw data before the
+      // actual relocation. Emit this data so that we are at the point where the
+      // relocation starts.
+      if (currentOffset < relocation.Offset) {
+        streamer->emitBytes(
+            FunctionContents.slice(currentOffset, relocation.Offset));
+        currentOffset = relocation.Offset;
+      }
+
+      currentOffset += relocation.emit(streamer);
+    }
+
+    // If we have emitted the last relocation, but there is still space before
+    // the specified end point. This simply means we have raw data at the end.
+    if (currentOffset < targetEndOffset) {
+      streamer->emitBytes(
+          FunctionContents.slice(currentOffset, targetEndOffset));
+      currentOffset = targetEndOffset;
+    }
+
+    return currentOffset;
+  };
+
+  streamer->emitCodeAlignment(
+      llvm::Align(function->getConstantIslandAlignment()),
+      &*binaryContext->STI);
+
+  // We are now going to be emitting the original constant islands, that are
+  // possibly spread out, into one big constant island at the end of the
+  // function. This label denotes the start of this island.
+  streamer->emitLabel(function->getFunctionConstantIslandLabel());
+
+  // Sequentially emit each island chunk one after the other.
+  for (const IslandChunk &chunk : islandChunks) {
+    uint64_t CurrentOffset = chunk.startOffset;
+
+    auto relocationsStart = chunk.relocations.begin();
+    const auto relocationsEnd = chunk.relocations.end();
+
+    // Emit the data and relocations between the currentOffset and the next jump
+    // label. Note that each sequence of data needs to start with a label. The
+    // first sequnece uses the constant island label as its start symbl. We then
+    // emit the label for the next sequence at the end of each loop.
+    for (const auto &[endOffset, symbol] : chunk.jumpLabels) {
+
+      // TODO this should ideally be constructed in place in the
+      // BuildIslandChunks. That way, we can place the constant island label as
+      // the label in the first sequence instead of adding it at the end like
+      // this. We would also be able to just loop everything nicely here.
+      //
+      // Find the relocations to emit from the range. We are
+      // essentially sliding a window across the range to account for the offset
+      // we are trying to emit.
+      const auto relocationSubRangeEnd =
+          std::ranges::lower_bound(relocationsStart, relocationsEnd, endOffset,
+                                   {}, &llvm::bolt::Relocation::Offset);
+      const auto relocations =
+          std::ranges::subrange(relocationsStart, relocationSubRangeEnd);
+
+      CurrentOffset = emitDataInRange(CurrentOffset, endOffset, relocations);
+
+      // In case this symbol has already been emitted else where. Emitting the
+      // same symbol twice leads to a crash.
+      if (symbol->isUndefined()) {
+        streamer->emitLabel(symbol);
+      }
+
+      // Update the start of the next range to be the end of the current
+      // subrange of relocations we just emitted.
+      relocationsStart = relocationSubRangeEnd;
+    }
+
+    // Emit the last data sequence.
+    CurrentOffset = emitDataInRange(
+        CurrentOffset, chunk.endOffset,
+        std::ranges::subrange(relocationsStart, relocationsEnd));
+  }
+}
 
 void EmitFunctionBody(llvm::bolt::BinaryContext *binaryContext,
                       llvm::bolt::BinaryFunction *function,
@@ -54,7 +262,11 @@ void EmitFunctionBody(llvm::bolt::BinaryContext *binaryContext,
     }
   }
 
-  // emitConstantIslands
+  // If the function contains function islands, gather them all up and emit them
+  // at the end of the function as one contiguous constant island.
+  if (function->hasIslandsInfo()) {
+    emitConstantIsland(binaryContext, function, streamer);
+  }
 }
 
 bool skipFunction(llvm::bolt::BinaryFunction *function) {
@@ -102,6 +314,11 @@ void emitFunctions(llvm::bolt::BinaryContext *binaryContext,
     streamer->emitELFSize(startSymbol, sizeExpr);
   }
 }
+
+// TODO, split the emit code into sub folders when done. For example:
+// Emit/Functions/EmitFunctions
+//                EmitConstantIslands
+//     /Data     /...
 
 Error EmitObjectFile(llvm::bolt::BinaryContext *binaryContext,
                      const std::string &outputPath) {
